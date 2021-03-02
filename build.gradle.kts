a4579c29f8e41b5495455e2cba6cb55e71160f64
Kraskaska <k.krasilnikov.2008@gmail.com>
2021-03-02 10:55:56 +0300

    Preparing to P2P

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,19 @@ version = "0.1-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+kotlin {
+    sourceSets {
+        val main by getting {
 
-
+            dependencies {
+                implementation("io.ktor:ktor-kotlinMultiplatform:1.5.1")
+            }
+        }
+    }
+    sourceSets["main"].apply {
+        kotlin.srcDir("src/main/kotlin/")
+    }
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
